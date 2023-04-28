6ba43c6a1574ae84ef85b3e84b70ae4b25daa03a
swapnil <swapnilsrivastava92.ss@gmail.com>
2023-04-28 14:28:25 +0530

    This is jyothi's commit

---
+++ src/test/java/com/demo/FirstJenkinsTest.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstJenkinsTest {
+	
+	@Test
+	public void firstTest() {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 driver.get("https://tutorialsninja.com/demo/");
+		 Assert.assertTrue(driver.getTitle().contains("Store"),"Title is not displayed");
+		 driver.quit();
+	}
+
+}
